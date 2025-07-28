f6e56f7f61bbd6742352da0d3837cc4e5f3c2573
Viva <happybird00@naver.com>
2025-07-28 15:45:44 +0900

    test

--- src/main/java/com/example/demo/CicdController.java
+++ src/main/java/com/example/demo/CicdController.java
@@ -1,2 +1,14 @@
-package com.example.demo;public class CicdController {
+package com.example.demo;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CicdController {
+
+    @GetMapping("/hi")
+    public String demo() {
+        return "안녕 이게 되네 이게 접속이 되나봐";
+    }
 }
